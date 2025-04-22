458d1258175083ae2851db7f331e7b22b3310e30
akashsonu0 <akashsonu7744@gmail.com>
2025-04-22 15:01:47 +0530

    @Query annotation

---
+++ 23-spring-boot-jparepository-custom-query-HQL/src/main/java/in/pwskills/akash/dao/IEmployeeRepository.java
@@ -0,0 +1,26 @@
+package in.pwskills.akash.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import in.pwskills.akash.entity.Employee;
+
+public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	//SQL: 1. fetch one column data
+	@Query("FROM Employee Where empSal>=:esal")
+	public List<Employee> fetchAllEmployees(Double esal);
+	
+	//SQL: 2. select all columns
+	@Query("SELECT empName FROM Employee")
+	public List<String> fetchNameOfEmployees();
+	
+	@Query("SELECT empId,empName FROM Employee")
+	public List<Object[]> fetchIdAndNameofEmployee();
+	
+	@Query("SELECT empId,empDept FROM Employee WHERE empDept =:dept")
+	public List<Object[]> fetchBasedonDept(String dept);
+	
+}
---
+++ 23-spring-boot-jparepository-custom-query-HQL/src/main/java/in/pwskills/akash/runner/EmployeeRunner.java
@@ -0,0 +1,56 @@
+package in.pwskills.akash.runner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import in.pwskills.akash.dao.IEmployeeRepository;
+import in.pwskills.akash.entity.Employee;
+
+@Component
+public class EmployeeRunner implements CommandLineRunner {
+	
+	@Autowired
+	private IEmployeeRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		repo.saveAll(
+				Arrays.asList(
+						new Employee(101, "AA" , 200.0, "DEV"),
+						new Employee(102, "BA" , 500.0, "DEV"),
+						new Employee(103, "AC" , 200.0, "QA"),
+						new Employee(104, "DD" , 400.0, "QA"),
+						new Employee(105, "EF" , 600.0, "DEV")
+						)
+				);
+		
+		repo.fetchAllEmployees(300.0).forEach(System.out::println);
+		
+		repo.fetchNameOfEmployees().forEach(System.out::println);
+		
+		List<Object[]> objects = repo.fetchIdAndNameofEmployee();
+		for(Object[] obj : objects) {
+			for(Object row: obj) {
+				System.out.print(row + "\t");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("****************************");
+		
+		List<Object[]> objs = repo.fetchBasedonDept("DEV");
+		for(Object[] obj : objs) {
+			for(Object row: obj) {
+				System.out.print(row + "\t");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
