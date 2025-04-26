10ce69468bd80c3747341c328106d4317df4fd31
akashsonu0 <akashsonu7744@gmail.com>
2025-04-26 14:54:14 +0530

    no @CollectionTable,@MapKeyColumn,@OrderColumn

---
+++ 25-spring-boot-jparepository-collection-mapping-optionalannotation/src/main/java/in/pwskills/akash/dao/IProductRepository.java
@@ -0,0 +1,12 @@
+package in.pwskills.akash.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.pwskills.akash.entity.Product;
+
+public interface IProductRepository extends JpaRepository<Product, Integer> {
+
+	
+	
+	
+}
---
+++ 25-spring-boot-jparepository-collection-mapping-optionalannotation/src/main/java/in/pwskills/akash/entity/Product.java
@@ -0,0 +1,85 @@
+package in.pwskills.akash.entity;
+
+import java.io.Serializable;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="prodTab")
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="pid")
+	private Integer prodId;
+	
+	@Column(name="pcode")
+	private String prodCode;
+	
+	@ElementCollection
+	private List<String> colors;
+
+	@ElementCollection
+	private Set<String> models;
+
+	@ElementCollection
+	private Map<String, String> details;
+
+	public Product() {}
+
+	public Integer getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getProdCode() {
+		return prodCode;
+	}
+
+	public void setProdCode(String prodCode) {
+		this.prodCode = prodCode;
+	}
+
+	public List<String> getColors() {
+		return colors;
+	}
+
+	public void setColors(List<String> colors) {
+		this.colors = colors;
+	}
+
+	public Set<String> getModels() {
+		return models;
+	}
+
+	public void setModels(Set<String> models) {
+		this.models = models;
+	}
+
+	public Map<String, String> getDetails() {
+		return details;
+	}
+
+	public void setDetails(Map<String, String> details) {
+		this.details = details;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [prodId=" + prodId + ", prodCode=" + prodCode + ", colors=" + colors + ", models=" + models
+				+ ", details=" + details + "]";
+	}
+}
