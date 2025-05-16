5dffa31f43fda095532365e1e824bdc52516e529
akashsonu0 <akashsonu7744@gmail.com>
2025-05-16 22:03:16 +0530

    Lombok test

---
+++ LombokTestProject/src/in/pwskills/akash/TestApp.java
@@ -0,0 +1,19 @@
+package in.pwskills.akash;
+
+import in.pwskills.akash.bean.Employee;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		 Employee employee = new Employee();
+		 employee.setEid(10);
+		 employee.setEname("sachin");
+		 employee.setEaddress("MI");
+		 
+		 System.out.println(employee);
+		 
+		 Employee employee2 = new Employee(7,"dhoni","CSK");
+		 System.out.println(employee2);
+	}
+
+}
---
+++ LombokTestProject/src/in/pwskills/akash/bean/Employee.java
@@ -0,0 +1,22 @@
+package in.pwskills.akash.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	private Integer eid;
+	private String ename;
+	private String eaddress;
+	
+	
+	
+	
+}
