f4f5cb6255e08182e71284038bfeb53466d5aa47
akashsonu0 <akashsonu7744@gmail.com>
2026-09-25 15:36:47 +0530

    XML-Configuration

--- 01-spring-boot-DI (1)/01-soring-boot-DI/src/main/java/in/pwskills/akash/Application.java
+++ 01-spring-boot-DI (1)/01-soring-boot-DI/src/main/java/in/pwskills/akash/Application.java
@@ -1,14 +1,14 @@
 package in.pwskills.akash;
 
+import java.time.LocalDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Bean;
+import in.pwskills.akash.bean.WishMessageGenerator;
 
-import in.pwskills.akash.bean.Student;
 @SpringBootApplication
-@ImportResource("in/pwskills/akash/cfgs/applicationContext.xml")
 public class Application {
 
     static {
@@ -18,21 +18,27 @@ public class Application {
     public Application() {
         System.out.println("Application Object is created...");
     }
-    
+
+    @Bean(name="ldt")
+    public LocalDateTime ldtObj() {
+    	System.out.println("Bean created with name :: ldt ->Application.ldtObj");
+        return LocalDateTime.now();
+    }
+
     public static void main(String[] args) {
 
         // Start the container
     	System.out.println("*************Conatiner Started*************");
         ApplicationContext context = SpringApplication.run(Application.class, args);
-        
         System.out.println("Name of the container is :: " + context.getClass().getName());
-        Student std = context.getBean(Student.class);
-        std.preparation("CTS-INTERVIEW");
-      
-        
+
+        // Get Bean
+        WishMessageGenerator wmg = context.getBean(WishMessageGenerator.class);
+        String status = wmg.WishMessage("Sachin");
+        System.out.println(status);
+
         // Close the container
         ((ConfigurableApplicationContext) context).close();
         System.out.println("*************Conatiner Stopped*************");
     }
-    
 }
